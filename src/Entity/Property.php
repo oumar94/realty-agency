45224a5914b2f4e59e822a013ba41f0d89b7ff9e
omarsow.alpha <Omarsow.Alpha@ext.cdiscount.com>
2020-01-17 18:20:27 +0100

    feat:validation des donnees

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -34,6 +37,7 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(min = 10,max = 400)
      */
     private $surface;
 
@@ -69,6 +73,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/[0-9]{5}/")
      */
     private $postal_code;
 
